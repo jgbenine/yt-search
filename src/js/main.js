8e5144f8d01a9945378e68de2be2e59071febda4
jgbenine <joaobenine@gmail.com>
2024-06-27 01:14:46 -0300

    init project

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// import ReactDOM from 'react-dom'.
+
+import helloWorld from './modules/helloWorld';
+
+
+
+console.log(helloWorld());
+
+// console.log('dae')
+
+// Para usar React, descomente:
+// ReactDOM.render(<div> Isso aqui é uma app React </div>, document.getElementById('react-app'))
