ea8ce5e966505db7f191366783394bf32e32a6d7
jgbenine <joaobenine@gmail.com>
2024-06-28 22:22:53 -0300

    feat: cria estrutura html p/ videos

---
+++ src/js/modules/createDisplayVideos.ts
@@ -0,0 +1,37 @@
+import { VideosType } from "../../@types/videos";
+import { handleFavoriteClick } from "./favorites/handleFavorites";
+import { isFavorite } from "./favorites/isFavorites";
+import { Modal } from "./modal";
+
+export function displayVideos(videos: VideosType[]) {
+  const container = document.querySelector(".content__grid");
+  container.innerHTML = "";
+
+  videos.forEach((video) => {
+    const videoElement = document.createElement("ul");
+    videoElement.classList.add("card-video");
+    videoElement.innerHTML = `
+      <li class="card-video__item">
+         <div class="card-video__wrapperImg">
+          <img src="${video.thumbnail}" alt="${video.title}">
+        </div>
+        <article class="card-video__texts">
+          <h2 class="card-video__title">${video.title}</h2>
+          <p class="card-video__chanel">${video.chanelTitle}</p>
+          <span class="card-video__views">${video.views.toLocaleString()} views</span>
+        </article>
+        <button class="card-video__favorite">
+          <i class="fa-solid fa-star card-video__icon ${isFavorite(video.url) ? 'card-video__icon--active' : ''}"></i>
+        </button>
+      </li> 
+    `;
+    
+    const favoriteButton = videoElement.querySelector(".card-video__favorite") as HTMLButtonElement;
+    favoriteButton.addEventListener("click", (event) => {
+      handleFavoriteClick(event, videoElement, video.url);
+    });
+
+    Modal(video.url, videoElement);
+    container.appendChild(videoElement);
+  });
+}
